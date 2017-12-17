ca7f00acbab64921c9d7913fae06b2c9f9727281
eastroots92 <eastroots92@gmail.com>
2017-12-17 17:56:09 +0900

    카테고리를 위한 구분 및 명칭 변경

--- app/util/Coin.rb
+++ app/util/Coin.rb
@@ -3,7 +3,64 @@ require 'open-uri'
 class Coin
     # 이름 변경해야함
     # value 소문자로 변경 해야함 coinnest
-    def self.coinnest 
+
+    def self.exchangeStore
+        grouped_options ={
+            '거래소' =>[
+                ['코인네스트',"coinnest"],
+                ['업비트','upbit'],
+            ]
+        }
+
+        return grouped_options
+    end
+
+    def self.coinnest
+
+        grouped_options = {
+            '코인네스트' => [
+                ['카이버(KNC)',"knc"],
+                ["에너고(TSL)", "tsl"],
+                ["트론(TRON)" ,"tron"],
+                ["비트코인(BTC)" ,"btc"],
+                ["비트코인캐쉬(BCH)" ,"btc"],
+                ["비트코인골드(BTG)" ,"btg"],
+                ["비트코인다이아(BCD)" ,"bcd"],
+                ["이더리움(ETH)", "eth"],
+                ["이더리움클래식(ETC)" ,"etc"],
+                ["카르다노(ADA)", "ada"],
+                ["큐텀(QTUM)","qtum"],
+                ["네오(NEO)","neo"],
+                ["NeoGas(GAS)", "gas"],
+                ["OmiseGO(OMG)" ,"omg"],
+                ["윌튼(WTC)" ,"wtc"],
+                ["모나코(MCO)" ,"mco"],
+                ["Ink(INK)" ,"ink"]
+            ]
+        }
+        return grouped_options
+    end
+
+    def self.upbit
+        grouped_options ={
+            '업비트' => [
+                ["비트코인BTC/KRW","KRW-BTC"]  ,     
+                ["퀀텀QTUM/KRW","KRW-QTUM"],
+                ["리플XRP/KRW","KRW-XRP"],
+                ["아인스타이늄EMC2/KRW","KRW-EMC2"],
+                ["에이다ADA/KRW","KRW-ADA"],
+                ["스테이터스네트워크토큰SNT/KRW","KRW-SNT"],
+                ["이더리움클래식ETC/KRW","KRW-ETC"],
+                ["스텔라루멘XLM/KRW","KRW-XLM"],
+                ["라이트코인LTC/KRW","KRW-LTC"]
+            ]
+
+        }
+        return grouped_options
+    end
+
+
+    def self.coinnestssssss
         grouped_options = 
         {
             'COINNEST' => [
